81989beaa7ce956b3404932f1d5758b3f33ac74b
Jacob Kuehl <jacob.kuehl@my.simpson.edu>
2021-09-06 12:32:39 -0500

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,4 @@
-print("hi")
+print("Hello darkness my old friend, I've come to talk with you again.")
+print("Country roads take me home, to the place, I belong. West Virginia, mountain momma,")
+print("take me home, down country roads.")
+print("'Hey, look what I can do\' \\!")
