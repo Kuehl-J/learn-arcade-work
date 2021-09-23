2b29542a2531a9a85879251167f1e1e187ffd2ef
Jacob Kuehl <jacob.kuehl@my.simpson.edu>
2021-09-23 15:39:17 -0500

    (lab_04.py)

--- Lab 04 - Camel/lab_04.py
+++ Lab 04 - Camel/lab_04.py
@@ -1,6 +1,26 @@
-repititions = int(input("How many times?"))
+import random
 
-for i in range(5):
-    print("I will not chew gum in class.")
+def main():
 
-    print("But I can drink water.")
+    print("You have stolen and Empire speeder on the moon planet of Endor.")
+    print("You must make it 200 miles to reach your allies and get off the planet.")
+    print("If the Empire catches you they will take you back to their base and imprison you.")
+
+    done = False
+    while not done:
+        print("A. Travel at slow speed")
+        print("B. Travel at moderate speed")
+        print("C. Travel at max speed")
+        print("D. Use boosters")
+        print("E. Use Force Senses")
+
+    user.choice = input("What do you do?")
+    elif user.choice.lower() == "a":
+        print("You travel slowly but safely. The Empire is catching up!")
+
+    elif user.choice.lower() =="b":
+        print("You travel at a decent pace, Endor filled with large trees.")
+
+
+
+main()
--- Testing/test.py
+++ Testing/test.py
@@ -1,8 +1,8 @@
-temperature = int(input("What is the temperature in Fahrenheit? "))
-if temperature > 90:
-    print("It is hot outside")
-elif temperature < 30:
-    print("It is cold outside")
-else:
-    print("It is not hot outside")
-print("Done")
+import random
+
+for i in range(20):
+    my_number = random.randrange(5)
+    if my_number == 0:
+        print("Dragon!")
+    else:
+        print(my_number)
